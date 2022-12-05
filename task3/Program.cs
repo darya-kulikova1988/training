a0054c227b3894660819b4ffe480a1252a249668
Darya <darya.kulikova88@yandex.ru>
2022-12-05 21:24:34 +0300

    task3

--- task3/Program.cs
+++ task3/Program.cs
@@ -3,18 +3,18 @@
 
 
 Console.WriteLine("Введите первое число: ");
-int a = int.Parse(Console.ReadLine());
+int a = int.Parse(Console.ReadLine()!);
 Console.WriteLine("Введите второе число: ");
-int b = int.Parse(Console.ReadLine());
-// Console.WriteLine("Введите третье число: ");
-// int c = Console.ReadLine()!;
-// Console.WriteLine("Введите четвертое число: ");
-// int d = Console.ReadLine()!;
+int b = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите третье число: ");
+int c = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите четвертое число: ");
+int d = int.Parse(Console.ReadLine()!);
 int divider = 1;
 int max = divider;
-while (divider<=a | divider<=b)
+while (divider<=a | divider<=b | divider<=c | divider<=d)
     {
-        if (a%divider==0 & b%divider==0)
+        if (a%divider==0 & b%divider==0 & c%divider==0 & d%divider==0)
         {
             if(divider>max) 
             {max = divider;}
