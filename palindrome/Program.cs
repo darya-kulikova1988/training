c2b3affc504ca8e5208b03dd39cc6d754e8ff65c
Darya <darya.kulikova88@yandex.ru>
2022-12-05 19:02:00 +0300

    palindrome

---
+++ palindrome/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите слово: ");
+string word = Console.ReadLine()!;
+char[] ch = new char[word.Length];
+for (int i = 0; i < word.Length; i++) 
+{ 
+ch[i] = word[i];
+}
+int j = 0;
+int n = word.Length;
+int count = 0;
+while (j < n)
+{if (ch[j]==ch[n-j-1])
+{
+    count++;
+    j++;
+        if (count == n)
+{Console.WriteLine(word + " является палиндромом");}
+}
+    else
+{Console.WriteLine(word + " не является палиндромом");
+    break;}}
